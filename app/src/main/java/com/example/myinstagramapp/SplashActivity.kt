919a27545cfcd59600dd1a519e37a1cafe0a8e94
LumyHiga <lumyhiga@gmail.com>
2020-04-06 16:13:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myinstagramapp/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myinstagramapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+
+    }
+}
